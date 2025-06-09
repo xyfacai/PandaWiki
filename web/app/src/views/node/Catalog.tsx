8c6a57ad6b0a7bba894771859c4a41f467a7948d
yu.kuai <yu.kuai@chaitin.com>
2025-06-09 01:57:49 +0000

    feat(fe/app): 支持目录收起展开

--- web/app/src/views/node/Catalog.tsx
+++ web/app/src/views/node/Catalog.tsx
@@ -1,49 +1,12 @@
 'use client'
 
-import { ITreeItem, NodeListItem } from "@/assets/type";
-import { IconFile, IconFolder } from "@/components/icons";
+import { NodeListItem } from "@/assets/type";
 import { convertToTree } from "@/utils/drag";
-import { Box, Stack } from "@mui/material";
-import { Ellipsis } from "ct-mui";
+import { Box } from "@mui/material";
+import CatalogFolder from "./CatalogFolder";
 
 const Catalog = ({ nodes, activeId, onChange }: { nodes: NodeListItem[], activeId: string, onChange: (id: string) => void }) => {
   const tree = convertToTree(nodes)
-
-  const renderNode = (item: ITreeItem) => (
-    <Box key={item.id}>
-      <Box sx={{
-        lineHeight: '32px',
-        cursor: 'pointer',
-        color: activeId === item.id ? 'primary.main' : 'inherit',
-        fontWeight: activeId === item.id ? 'bold' : 'normal',
-        '&:hover': { color: 'primary.main' }
-      }}>
-        <Stack direction="row" alignItems="center" gap={1}>
-          {item.type === 1 ? <IconFolder sx={{ flexShrink: 0 }} /> : <IconFile sx={{ flexShrink: 0 }} />}
-          {item.type === 2 ? <Box sx={{ flex: 1, width: 0 }}>
-            <Ellipsis onClick={(event) => {
-              event.stopPropagation()
-              onChange(item.id)
-              window.history.pushState(null, '', `/node/${item.id}`)
-            }}>
-              {item.name}
-            </Ellipsis>
-          </Box> : <Box sx={{ flex: 1, width: 0 }}>
-            <Ellipsis>
-              {item.name}
-            </Ellipsis>
-          </Box>}
-        </Stack>
-      </Box>
-      {item.children && item.children.length > 0 && (
-        <Box sx={{ ml: 2.5 }}>
-          {item.children.map((child) =>
-            renderNode(child)
-          )}
-        </Box>
-      )}
-    </Box>
-  )
 
   return <Box sx={{
     width: 250,
@@ -70,7 +33,7 @@ const Catalog = ({ nodes, activeId, onChange }: { nodes: NodeListItem[], activeI
       msOverflowStyle: 'none',
       scrollbarWidth: 'none',
     }}>
-      {tree.map((item) => renderNode(item))}
+      {tree.map((item) => <CatalogFolder key={item.id} item={item} activeId={activeId} onChange={onChange} />)}
     </Box>
   </Box>
 };
---
+++ web/app/src/views/node/CatalogFolder.tsx
@@ -0,0 +1,50 @@
+import { ITreeItem } from "@/assets/type"
+import { IconArrowDown, IconFile, IconFolder } from "@/components/icons"
+import { Box, Stack } from "@mui/material"
+import { Ellipsis } from "ct-mui"
+import { useState } from "react"
+
+const CatalogFolder = ({ item, activeId, onChange }: { item: ITreeItem, activeId: string, onChange: (id: string) => void }) => {
+  const [isExpanded, setIsExpanded] = useState(true)
+
+  return <Box key={item.id}>
+    <Box sx={{
+      lineHeight: '32px',
+      cursor: 'pointer',
+      color: activeId === item.id ? 'primary.main' : 'inherit',
+      fontWeight: activeId === item.id ? 'bold' : 'normal',
+      '&:hover': { color: 'primary.main' }
+    }} onClick={() => {
+      setIsExpanded(!isExpanded)
+    }}>
+      <Stack direction="row" alignItems="center" gap={1}>
+        {item.type === 1 && <Box sx={{ flexShrink: 0 }}>
+          <IconArrowDown sx={{ transform: isExpanded ? 'none' : 'rotate(-90deg)', transition: 'transform 0.2s' }} />
+        </Box>}
+        {item.type === 1 ? <IconFolder sx={{ flexShrink: 0 }} /> : <IconFile sx={{ flexShrink: 0 }} />}
+        {item.type === 2 ? <Box sx={{ flex: 1, width: 0 }}>
+          <Ellipsis onClick={(event) => {
+            event.stopPropagation()
+            onChange(item.id)
+            window.history.pushState(null, '', `/node/${item.id}`)
+          }}>
+            {item.name}
+          </Ellipsis>
+        </Box> : <Box sx={{ flex: 1, width: 0 }}>
+          <Ellipsis>
+            {item.name}
+          </Ellipsis>
+        </Box>}
+      </Stack>
+    </Box>
+    {item.children && item.children.length > 0 && isExpanded && (
+      <Box sx={{ ml: 2.5 }}>
+        {item.children.map((child) =>
+          <CatalogFolder key={child.id} item={child} activeId={activeId} onChange={onChange} />
+        )}
+      </Box>
+    )}
+  </Box>
+}
+
+export default CatalogFolder
