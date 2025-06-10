4de3f93e501e1f3b44c37a00ff762cd4125876ed
yu.kuai <yu.kuai@chaitin.com>
2025-06-10 12:24:27 +0000

    feat(fe/app): 新增更新时间

--- web/app/src/components/icons/index.tsx
+++ web/app/src/components/icons/index.tsx
@@ -119,3 +119,17 @@ export const IconClose = (props: SvgIconProps) => {
 }
 
 IconClose.displayName = 'icon-close';
+
+export const IconClock = (props: SvgIconProps) => {
+  return <SvgIcon
+    viewBox="0 0 1024 1024"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M512 93.12A418.88 418.88 0 1 0 930.88 512 419.328 419.328 0 0 0 512 93.12z m0 753.92a335.104 335.104 0 1 1 335.104-335.104A335.488 335.488 0 0 1 512 847.04z"></path>
+    <path d="M512 575.936a44.8 44.8 0 0 1-44.8-44.8V327.104a44.8 44.8 0 0 1 44.8-44.8 44.8 44.8 0 0 1 44.8 44.8V486.4h96a44.8 44.8 0 0 1 44.8 44.8 44.8 44.8 0 0 1-44.8 44.8z"></path>
+  </SvgIcon>
+}
+
+IconClock.displayName = 'icon-clock';
