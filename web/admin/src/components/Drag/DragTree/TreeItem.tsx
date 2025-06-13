5c38d28225b431bf7b2386163ad8c4d8657964e5
holly <yu.kuai@chaitin.com>
2025-06-13 22:32:34 +0800

    Update TreeItem.tsx

--- web/admin/src/components/Drag/DragTree/TreeItem.tsx
+++ web/admin/src/components/Drag/DragTree/TreeItem.tsx
@@ -54,7 +54,8 @@ const TreeItem = React.forwardRef<HTMLDivElement, TreeItemComponentProps<ITreeIt
         label: '创建文件夹',
         key: 'folder',
         onClick: () => {
-          setItems(items.map(i => (i.id === item.id) ? ({
+          const temp = [...items];
+          updateTree(temp, item.id, {
             ...item,
             children: [
               ...(item.children ?? []),
@@ -67,27 +68,30 @@ const TreeItem = React.forwardRef<HTMLDivElement, TreeItemComponentProps<ITreeIt
                 parentId: item.id,
               }
             ]
-          }) : i))
+          });
+          setItems(temp);
         }
       },
       {
         label: '创建文档',
         key: 'doc',
         onClick: () => {
-          setItems(items.map(i => (i.id === item.id) ? ({
-            ...item,
-            children: [
-              ...(item.children ?? []),
-              {
-                id: `${items.length + 10}`,
-                name: "",
-                level: 2,
-                type: 2,
-                isEditting: true,
-                parentId: item.id,
-              }
-            ]
-          }) : i))
+          const temp = [...items];
+           updateTree(temp, item.id, {
+              ...item,
+              children: [
+                ...(item.children ?? []),
+                {
+                  id: `${items.length + 10}`,
+                  name: "",
+                  level: 2,
+                  type: 2,
+                  isEditting: true,
+                  parentId: item.id,
+                }
+              ]
+           });
+           setItems(temp);
         }
       },
       {
