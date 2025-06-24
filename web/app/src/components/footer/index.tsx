7f3c3642881ccdf767a16947e8bf472b4e9f75b0
yu.kuai <yu.kuai@chaitin.com>
2025-06-24 06:14:33 +0000

    fix(fe/app): footer ui

--- web/app/src/components/footer/index.tsx
+++ web/app/src/components/footer/index.tsx
@@ -1,7 +1,7 @@
 import logo from '@/assets/images/logo.png';
 import { useKBDetail } from '@/provider/kb-provider';
 import { useMobile } from '@/provider/mobile-provider';
-import { Box, Stack } from "@mui/material";
+import { Box, Divider, Stack } from "@mui/material";
 import Image from "next/image";
 import Link from 'next/link';
 
@@ -19,7 +19,7 @@ const Footer = ({ showBrand = true }: { showBrand?: boolean }) => {
   }}>
     {footerSetting?.footer_style === 'complex' && showBrand && <Box sx={{ py: 5 }}>
       <Stack direction={'row'} flexWrap={'wrap'} gap={2} sx={{ mb: 5 }}>
-        {footerSetting?.brand_groups.map((group, idx) => (
+        {footerSetting?.brand_groups?.map((group, idx) => (
           <Stack gap={2} key={group.name} sx={{
             fontSize: 14,
             lineHeight: '22px',
@@ -39,7 +39,7 @@ const Footer = ({ showBrand = true }: { showBrand?: boolean }) => {
         ))}
       </Stack>
       <Stack direction={'row'} alignItems={'center'} gap={1}>
-        {footerSetting?.brand_logo && <Image src={footerSetting.brand_logo} alt="PandaWiki" width={24} height={24} />}
+        {footerSetting?.brand_logo && <img src={footerSetting.brand_logo} alt="PandaWiki" width={24} height={24} />}
         <Box sx={{ fontWeight: 'bold', lineHeight: '32px', fontSize: 24 }}>{footerSetting?.brand_name}</Box>
       </Stack>
       {footerSetting?.brand_desc && <Box sx={{ fontSize: 12, color: 'text.secondary', lineHeight: '26px', ml: footerSetting?.brand_logo ? 4 : 0, mt: 2 }}>
@@ -79,7 +79,7 @@ const Footer = ({ showBrand = true }: { showBrand?: boolean }) => {
       <Stack direction={'row'} justifyContent={'space-between'} gap={10}>
         <Box sx={{ width: '30%', minWidth: 200 }}>
           {footerSetting?.brand_name && <Stack direction={'row'} alignItems={'center'} gap={1} sx={{ mb: 2 }}>
-            {footerSetting?.brand_logo && <Image src={footerSetting.brand_logo} alt="PandaWiki" width={24} height={24} />}
+            {footerSetting?.brand_logo && <img src={footerSetting.brand_logo} alt="PandaWiki" width={24} height={24} />}
             <Box sx={{ fontWeight: 'bold', lineHeight: '32px', fontSize: 24 }}>{footerSetting?.brand_name}</Box>
           </Stack>}
           {footerSetting?.brand_desc && <Box sx={{ fontSize: 12, color: 'text.secondary', lineHeight: '26px' }}>
@@ -87,7 +87,7 @@ const Footer = ({ showBrand = true }: { showBrand?: boolean }) => {
           </Box>}
         </Box>
         <Stack direction={'row'} justifyContent={'flex-end'}>
-          {footerSetting?.brand_groups.map((group) => (
+          {footerSetting?.brand_groups?.map((group) => (
             <Stack gap={2} key={group.name} sx={{
               fontSize: 14,
               lineHeight: '22px',
@@ -113,7 +113,8 @@ const Footer = ({ showBrand = true }: { showBrand?: boolean }) => {
       <Stack direction={'row'} alignItems={'center'} justifyContent={'space-between'}>
         <Stack direction={'row'} alignItems={'center'} gap={1} sx={{ color: 'text.tertiary' }}>
           {!!footerSetting?.corp_name && <Box>© 2025 {footerSetting?.corp_name} 版权所有</Box>}
-          {!!footerSetting?.icp && <Box>{footerSetting?.icp}</Box>}
+          {!!footerSetting?.corp_name && !!footerSetting?.icp && !mobile && <Divider orientation='vertical' sx={{ mx: 0.5, height: 16 }} />}
+          {!!footerSetting?.icp && <Link href={`https://beian.miit.gov.cn/`} target='_blank'>{footerSetting?.icp}</Link>}
         </Stack>
         <Stack direction={'row'} alignItems={'center'} gap={0.5}>
           本网站由
