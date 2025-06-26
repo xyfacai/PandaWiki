9ad8395fdcae67aef320e52b0a69bf4632a5554a
xiaomakuaiz <jackli9527001@gmail.com>
2025-06-26 09:36:24 +0000

    fix: add default route last

--- backend/repo/pg/knowledge_base.go
+++ backend/repo/pg/knowledge_base.go
@@ -58,7 +58,7 @@ func (r *KnowledgeBaseRepository) SyncKBAccessSettingsToCaddy(ctx context.Contex
 	if len(firstKB.AccessSettings.Hosts) > 0 {
 		firstHost = firstKB.AccessSettings.Hosts[0]
 	}
-	pems := make([]map[string]any, 0)
+	certs := make([]map[string]any, 0)
 	portHostKBMap := make(map[string]map[string]*domain.KnowledgeBaseListItem)
 	httpPorts := make(map[string]struct{})
 	for _, kb := range kbList {
@@ -80,7 +80,7 @@ func (r *KnowledgeBaseRepository) SyncKBAccessSettingsToCaddy(ctx context.Contex
 			}
 		}
 		if len(kb.AccessSettings.PublicKey) > 0 && len(kb.AccessSettings.PrivateKey) > 0 {
-			pems = append(pems, map[string]any{
+			certs = append(certs, map[string]any{
 				"certificate": kb.AccessSettings.PublicKey,
 				"key":         kb.AccessSettings.PrivateKey,
 				"tags":        []string{kb.ID},
@@ -120,6 +120,7 @@ func (r *KnowledgeBaseRepository) SyncKBAccessSettingsToCaddy(ctx context.Contex
 			}
 		}
 		routes := make([]map[string]any, 0)
+		var defaultRoute map[string]any
 		for host, kb := range hostKBMap {
 			route := map[string]any{
 				"handle": []map[string]any{
@@ -224,8 +225,7 @@ func (r *KnowledgeBaseRepository) SyncKBAccessSettingsToCaddy(ctx context.Contex
 			if host == firstHost {
 				// first host as default host
 				// copy route without the host match
-				routeCopy := maps.Clone(route)
-				routes = append(routes, routeCopy)
+				defaultRoute = maps.Clone(route)
 			}
 			if host != "*" {
 				route["match"] = []map[string]any{
@@ -236,6 +236,10 @@ func (r *KnowledgeBaseRepository) SyncKBAccessSettingsToCaddy(ctx context.Contex
 			}
 			routes = append(routes, route)
 		}
+		// add default route if exists
+		if defaultRoute != nil {
+			routes = append(routes, defaultRoute)
+		}
 		server["routes"] = routes
 		servers[port] = server
 	}
@@ -244,10 +248,10 @@ func (r *KnowledgeBaseRepository) SyncKBAccessSettingsToCaddy(ctx context.Contex
 			"servers": servers,
 		},
 	}
-	if len(pems) > 0 {
+	if len(certs) > 0 {
 		apps["tls"] = map[string]any{
 			"certificates": map[string]any{
-				"load_pem": pems,
+				"load_pem": certs,
 			},
 		}
 	}
