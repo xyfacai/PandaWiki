0505d3b5ec288f25c0776db5f23c020a1ae3604f
Gavan <994259213@qq.com>
2025-08-07 10:48:17 +0800

    feat: 激活添加选择版本

--- web/admin/src/request/types.ts
+++ web/admin/src/request/types.ts
@@ -54,7 +54,6 @@ export enum DomainModelType {
 }
 
 export enum DomainModelProvider {
-  /** 智谱 */
   ModelProviderBrandOpenAI = "OpenAI",
   ModelProviderBrandOllama = "Ollama",
   ModelProviderBrandDeepSeek = "DeepSeek",
@@ -66,6 +65,7 @@ export enum DomainModelProvider {
   ModelProviderBrandBaiLian = "BaiLian",
   ModelProviderBrandVolcengine = "Volcengine",
   ModelProviderBrandGemini = "Gemini",
+  /** 智谱 */
   ModelProviderBrandZhiPu = "ZhiPu",
   ModelProviderBrandOther = "Other",
 }
@@ -490,6 +490,7 @@ export interface DomainCreateNodeReq {
   kb_id: string;
   name: string;
   parent_id?: string;
+  position?: number;
   type: 1 | 2;
   visibility?: DomainNodeVisibility;
 }
@@ -832,6 +833,19 @@ export interface DomainShareCommentListItem {
   root_id?: string;
 }
 
+export interface DomainShareConversationDetailResp {
+  created_at?: string;
+  id?: string;
+  messages?: DomainShareConversationMessage[];
+  subject?: string;
+}
+
+export interface DomainShareConversationMessage {
+  content?: string;
+  created_at?: string;
+  role?: SchemaRoleType;
+}
+
 export interface DomainSimpleAuth {
   enabled?: boolean;
   password?: string;
@@ -900,6 +914,7 @@ export interface DomainUpdateNodeReq {
   id: string;
   kb_id: string;
   name?: string;
+  position?: number;
   summary?: string;
   visibility?: DomainNodeVisibility;
 }
@@ -1177,6 +1192,11 @@ export interface PostShareV1ChatWidgetParams {
 
 export interface GetShareV1CommentListParams {
   /** nodeID */
+  id: string;
+}
+
+export interface GetShareV1ConversationDetailParams {
+  /** conversation id */
   id: string;
 }
 
