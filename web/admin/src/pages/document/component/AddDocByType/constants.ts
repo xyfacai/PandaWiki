0e4176489a802759bc1c46b287627e323b8fc163
yu.kuai <yu.kuai@chaitin.com>
2025-10-28 14:58:09 +0800

    feat: 导入文档支持保留文档树结构

--- web/admin/src/pages/document/component/AddDocByType/constants.ts
+++ web/admin/src/pages/document/component/AddDocByType/constants.ts
@@ -1,4 +1,4 @@
-import { ImportDocType } from '@/api';
+import { ConstsCrawlerSource } from '@/request';
 
 // 文档状态常量
 export const DOCUMENT_STATUS = {
@@ -22,35 +22,40 @@ export const ITEM_TYPE = {
   FOLDER: 'folder',
 } as const;
 
+export const NoParseTypes: readonly ConstsCrawlerSource[] = [
+  ConstsCrawlerSource.CrawlerSourceFile,
+  ConstsCrawlerSource.CrawlerSourceEpub,
+] as const;
+
 // 需要上传文件的导入类型
-export const UPLOAD_FILE_TYPES: readonly ImportDocType[] = [
-  'OfflineFile',
-  'Epub',
-  'Wiki.js',
-  'Yuque',
-  'Siyuan',
-  'MinDoc',
-  'Confluence',
+export const UPLOAD_FILE_TYPES: readonly ConstsCrawlerSource[] = [
+  ConstsCrawlerSource.CrawlerSourceFile,
+  ConstsCrawlerSource.CrawlerSourceEpub,
+  ConstsCrawlerSource.CrawlerSourceWikijs,
+  ConstsCrawlerSource.CrawlerSourceYuque,
+  ConstsCrawlerSource.CrawlerSourceSiyuan,
+  ConstsCrawlerSource.CrawlerSourceMindoc,
+  ConstsCrawlerSource.CrawlerSourceConfluence,
 ] as const;
 
 // 需要解析的导入类型
-export const PARSE_TYPES: readonly ImportDocType[] = [
-  'Confluence',
-  'Wiki.js',
-  'Siyuan',
-  'MinDoc',
-  'Notion',
+export const PARSE_TYPES: readonly ConstsCrawlerSource[] = [
+  ConstsCrawlerSource.CrawlerSourceConfluence,
+  ConstsCrawlerSource.CrawlerSourceWikijs,
+  ConstsCrawlerSource.CrawlerSourceSiyuan,
+  ConstsCrawlerSource.CrawlerSourceMindoc,
+  ConstsCrawlerSource.CrawlerSourceNotion,
 ] as const;
 
 // 需要抓取的导入类型
-export const SCRAPE_TYPES: readonly ImportDocType[] = [
-  'RSS',
-  'Sitemap',
+export const SCRAPE_TYPES: readonly ConstsCrawlerSource[] = [
+  ConstsCrawlerSource.CrawlerSourceRSS,
+  ConstsCrawlerSource.CrawlerSourceSitemap,
 ] as const;
 
 // 类型配置
 export const TYPE_CONFIG: Record<
-  ImportDocType,
+  ConstsCrawlerSource,
   {
     label: string;
     okText?: string;
@@ -58,70 +63,70 @@ export const TYPE_CONFIG: Record<
     usage?: string;
   }
 > = {
-  OfflineFile: {
+  [ConstsCrawlerSource.CrawlerSourceFile]: {
     label: '通过离线文件导入',
     okText: '导入文件',
     accept: '.txt, .md, .xls, .xlsx, .docx, .pdf, .html, .pptx',
     usage:
       'https://pandawiki.docs.baizhi.cloud/node/01976929-0e76-77a9-aed9-842e60933464#%E9%80%9A%E8%BF%87%E7%A6%BB%E7%BA%BF%E6%96%87%E4%BB%B6%E5%AF%BC%E5%85%A5',
   },
-  URL: {
+  [ConstsCrawlerSource.CrawlerSourceUrl]: {
     label: '通过 URL 导入',
     usage:
       'https://pandawiki.docs.baizhi.cloud/node/01976929-0e76-77a9-aed9-842e60933464#%E9%80%9A%E8%BF%87%20URL%20%E5%AF%BC%E5%85%A5',
   },
-  RSS: {
+  [ConstsCrawlerSource.CrawlerSourceRSS]: {
     label: '通过 RSS 导入',
     usage:
       'https://pandawiki.docs.baizhi.cloud/node/01976929-0e76-77a9-aed9-842e60933464#%E9%80%9A%E8%BF%87%20RSS%20%E5%AF%BC%E5%85%A5',
   },
-  Sitemap: {
+  [ConstsCrawlerSource.CrawlerSourceSitemap]: {
     label: '通过 Sitemap 导入',
     usage:
       'https://pandawiki.docs.baizhi.cloud/node/01976929-0e76-77a9-aed9-842e60933464#%E9%80%9A%E8%BF%87%20SiteMap%20%E5%AF%BC%E5%85%A5',
   },
-  Notion: {
+  [ConstsCrawlerSource.CrawlerSourceNotion]: {
     label: '通过 Notion 导入',
     usage:
       'https://pandawiki.docs.baizhi.cloud/node/01976929-0e76-77a9-aed9-842e60933464#%E9%80%9A%E8%BF%87%20Notion%20%E5%AF%BC%E5%85%A5',
   },
-  Epub: {
+  [ConstsCrawlerSource.CrawlerSourceEpub]: {
     label: '通过 Epub 导入',
     accept: '.epub',
     usage:
       'https://pandawiki.docs.baizhi.cloud/node/01976929-0e76-77a9-aed9-842e60933464#%E9%80%9A%E8%BF%87%20Epub%20%E5%AF%BC%E5%85%A5',
   },
-  'Wiki.js': {
+  [ConstsCrawlerSource.CrawlerSourceWikijs]: {
     label: '通过 Wiki.js 导入',
     accept: '.zip',
     usage:
       'https://pandawiki.docs.baizhi.cloud/node/01976929-0e76-77a9-aed9-842e60933464#%E9%80%9A%E8%BF%87%20Wiki.js%20%E5%AF%BC%E5%85%A5',
   },
-  Yuque: {
+  [ConstsCrawlerSource.CrawlerSourceYuque]: {
     label: '通过语雀导入',
     accept: '.lakebook',
     usage:
       'https://pandawiki.docs.baizhi.cloud/node/01976929-0e76-77a9-aed9-842e60933464#%E9%80%9A%E8%BF%87%E8%AF%AD%E9%9B%80%E5%AF%BC%E5%85%A5',
   },
-  Siyuan: {
+  [ConstsCrawlerSource.CrawlerSourceSiyuan]: {
     label: '通过思源笔记导入',
     accept: '.zip',
     usage:
       'https://pandawiki.docs.baizhi.cloud/node/01976929-0e76-77a9-aed9-842e60933464#%E9%80%9A%E8%BF%87%E6%80%9D%E6%BA%90%E7%AC%94%E8%AE%B0%E5%AF%BC%E5%85%A5',
   },
-  MinDoc: {
+  [ConstsCrawlerSource.CrawlerSourceMindoc]: {
     label: '通过 MinDoc 导入',
     accept: '.zip',
     usage:
       'https://pandawiki.docs.baizhi.cloud/node/01976929-0e76-77a9-aed9-842e60933464#%E9%80%9A%E8%BF%87%20MinDoc%20%E5%AF%BC%E5%85%A5',
   },
-  Feishu: {
+  [ConstsCrawlerSource.CrawlerSourceFeishu]: {
     label: '通过飞书文档导入',
     okText: '拉取知识库',
     usage:
       'https://pandawiki.docs.baizhi.cloud/node/01976929-0e76-77a9-aed9-842e60933464#%E9%80%9A%E8%BF%87%E9%A3%9E%E4%B9%A6%E6%96%87%E6%A1%A3%E5%AF%BC%E5%85%A5',
   },
-  Confluence: {
+  [ConstsCrawlerSource.CrawlerSourceConfluence]: {
     label: '通过 Confluence 导入',
     accept: '.zip',
     usage:
--- web/admin/src/request/Crawler.ts
+++ web/admin/src/request/Crawler.ts
@@ -13,107 +13,39 @@
 import httpRequest, { ContentType, RequestParams } from "./httpClient";
 import {
   DomainPWResponse,
-  GetApiV1CrawlerResultParams,
-  PostApiV1CrawlerConfluenceParsePayload,
-  PostApiV1CrawlerMindocParsePayload,
-  PostApiV1CrawlerSiyuanParsePayload,
-  PostApiV1CrawlerWikijsParsePayload,
-  V1ConfluenceParseResp,
-  V1ConfluenceScrapeReq,
-  V1ConfluenceScrapeResp,
+  V1CrawlerExportReq,
+  V1CrawlerExportResp,
+  V1CrawlerParseReq,
+  V1CrawlerParseResp,
+  V1CrawlerResultReq,
   V1CrawlerResultResp,
   V1CrawlerResultsReq,
   V1CrawlerResultsResp,
-  V1EpubParseReq,
-  V1EpubParseResp,
-  V1FeishuGetDocReq,
-  V1FeishuGetDocResp,
-  V1FeishuListCloudDocReq,
-  V1FeishuListCloudDocResp,
-  V1FeishuSearchWikiReq,
-  V1FeishuSearchWikiResp,
-  V1FeishuSpaceListReq,
-  V1FeishuSpaceListResp,
-  V1MindocParseResp,
-  V1MindocScrapeReq,
-  V1MindocScrapeResp,
-  V1NotionParseReq,
-  V1NotionParseResp,
-  V1NotionScrapeReq,
-  V1NotionScrapeResp,
-  V1RssParseReq,
-  V1RssParseResp,
-  V1RssScrapeReq,
-  V1RssScrapeResp,
-  V1ScrapeReq,
-  V1ScrapeResp,
-  V1SitemapParseReq,
-  V1SitemapParseResp,
-  V1SitemapScrapeReq,
-  V1SitemapScrapeResp,
-  V1SiyuanParseResp,
-  V1SiyuanScrapeReq,
-  V1SiyuanScrapeResp,
-  V1WikijsParseResp,
-  V1WikijsScrapeReq,
-  V1WikijsScrapeResp,
-  V1YuqueParseReq,
-  V1YuqueParseResp,
 } from "./types";
 
 /**
- * @description Parse Confluence Export File and return document list
+ * @description CrawlerExport
  *
  * @tags crawler
- * @name PostApiV1CrawlerConfluenceParse
- * @summary ConfluenceParse
- * @request POST:/api/v1/crawler/confluence/parse
+ * @name PostApiV1CrawlerExport
+ * @summary CrawlerExport
+ * @request POST:/api/v1/crawler/export
  * @response `200` `(DomainPWResponse & {
-    data?: V1ConfluenceParseResp,
+    data?: V1CrawlerExportResp,
 
 })` OK
  */
 
-export const postApiV1CrawlerConfluenceParse = (
-  data: PostApiV1CrawlerConfluenceParsePayload,
+export const postApiV1CrawlerExport = (
+  body: V1CrawlerExportReq,
   params: RequestParams = {},
 ) =>
   httpRequest<
     DomainPWResponse & {
-      data?: V1ConfluenceParseResp;
+      data?: V1CrawlerExportResp;
     }
   >({
-    path: `/api/v1/crawler/confluence/parse`,
-    method: "POST",
-    body: data,
-    type: ContentType.FormData,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description Scrape specific Confluence documents by ID
- *
- * @tags crawler
- * @name PostApiV1CrawlerConfluenceScrape
- * @summary ConfluenceScrape
- * @request POST:/api/v1/crawler/confluence/scrape
- * @response `200` `(DomainPWResponse & {
-    data?: V1ConfluenceScrapeResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerConfluenceScrape = (
-  body: V1ConfluenceScrapeReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1ConfluenceScrapeResp;
-    }
-  >({
-    path: `/api/v1/crawler/confluence/scrape`,
+    path: `/api/v1/crawler/export`,
     method: "POST",
     body: body,
     type: ContentType.Json,
@@ -122,268 +54,28 @@ export const postApiV1CrawlerConfluenceScrape = (
   });
 
 /**
- * @description EpubParse
+ * @description 解析文档树
  *
  * @tags crawler
- * @name PostApiV1CrawlerEpubParse
- * @summary EpubParse
- * @request POST:/api/v1/crawler/epub/parse
+ * @name PostApiV1CrawlerParse
+ * @summary 解析文档树
+ * @request POST:/api/v1/crawler/parse
  * @response `200` `(DomainPWResponse & {
-    data?: V1EpubParseResp,
+    data?: V1CrawlerParseResp,
 
 })` OK
  */
 
-export const postApiV1CrawlerEpubParse = (
-  body: V1EpubParseReq,
+export const postApiV1CrawlerParse = (
+  body: V1CrawlerParseReq,
   params: RequestParams = {},
 ) =>
   httpRequest<
     DomainPWResponse & {
-      data?: V1EpubParseResp;
+      data?: V1CrawlerParseResp;
     }
   >({
-    path: `/api/v1/crawler/epub/parse`,
-    method: "POST",
-    body: body,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description Get Docx in Feishu Spaces
- *
- * @tags crawler
- * @name PostApiV1CrawlerFeishuGetDoc
- * @summary FeishuDoc
- * @request POST:/api/v1/crawler/feishu/get_doc
- * @response `200` `(DomainPWResponse & {
-    data?: V1FeishuGetDocResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerFeishuGetDoc = (
-  body: V1FeishuGetDocReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1FeishuGetDocResp;
-    }
-  >({
-    path: `/api/v1/crawler/feishu/get_doc`,
-    method: "POST",
-    body: body,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description List Docx in Feishu Spaces
- *
- * @tags crawler
- * @name PostApiV1CrawlerFeishuListDoc
- * @summary FeishuListCloudDoc
- * @request POST:/api/v1/crawler/feishu/list_doc
- * @response `200` `(DomainPWResponse & {
-    data?: (V1FeishuListCloudDocResp)[],
-
-})` OK
- */
-
-export const postApiV1CrawlerFeishuListDoc = (
-  body: V1FeishuListCloudDocReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1FeishuListCloudDocResp[];
-    }
-  >({
-    path: `/api/v1/crawler/feishu/list_doc`,
-    method: "POST",
-    body: body,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description List All Feishu Spaces
- *
- * @tags crawler
- * @name PostApiV1CrawlerFeishuListSpaces
- * @summary FeishuListSpaces
- * @request POST:/api/v1/crawler/feishu/list_spaces
- * @response `200` `(DomainPWResponse & {
-    data?: (V1FeishuSpaceListResp)[],
-
-})` OK
- */
-
-export const postApiV1CrawlerFeishuListSpaces = (
-  body: V1FeishuSpaceListReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1FeishuSpaceListResp[];
-    }
-  >({
-    path: `/api/v1/crawler/feishu/list_spaces`,
-    method: "POST",
-    body: body,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description Search Wiki in Feishu Spaces
- *
- * @tags crawler
- * @name PostApiV1CrawlerFeishuSearchWiki
- * @summary FeishuWikiSearch
- * @request POST:/api/v1/crawler/feishu/search_wiki
- * @response `200` `(DomainPWResponse & {
-    data?: (V1FeishuSearchWikiResp)[],
-
-})` OK
- */
-
-export const postApiV1CrawlerFeishuSearchWiki = (
-  body: V1FeishuSearchWikiReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1FeishuSearchWikiResp[];
-    }
-  >({
-    path: `/api/v1/crawler/feishu/search_wiki`,
-    method: "POST",
-    body: body,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description Parse Mindoc Export File and return document list
- *
- * @tags crawler
- * @name PostApiV1CrawlerMindocParse
- * @summary MindocParse
- * @request POST:/api/v1/crawler/mindoc/parse
- * @response `200` `(DomainPWResponse & {
-    data?: V1MindocParseResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerMindocParse = (
-  data: PostApiV1CrawlerMindocParsePayload,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1MindocParseResp;
-    }
-  >({
-    path: `/api/v1/crawler/mindoc/parse`,
-    method: "POST",
-    body: data,
-    type: ContentType.FormData,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description Scrape specific Mindoc documents by ID
- *
- * @tags crawler
- * @name PostApiV1CrawlerMindocScrape
- * @summary MindocScrape
- * @request POST:/api/v1/crawler/mindoc/scrape
- * @response `200` `(DomainPWResponse & {
-    data?: V1MindocScrapeResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerMindocScrape = (
-  body: V1MindocScrapeReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1MindocScrapeResp;
-    }
-  >({
-    path: `/api/v1/crawler/mindoc/scrape`,
-    method: "POST",
-    body: body,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description NotionParse
- *
- * @tags crawler
- * @name PostApiV1CrawlerNotionParse
- * @summary NotionParse
- * @request POST:/api/v1/crawler/notion/parse
- * @response `200` `(DomainPWResponse & {
-    data?: V1NotionParseResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerNotionParse = (
-  body: V1NotionParseReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1NotionParseResp;
-    }
-  >({
-    path: `/api/v1/crawler/notion/parse`,
-    method: "POST",
-    body: body,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description NotionScrape
- *
- * @tags crawler
- * @name PostApiV1CrawlerNotionScrape
- * @summary NotionScrape
- * @request POST:/api/v1/crawler/notion/scrape
- * @response `200` `(DomainPWResponse & {
-    data?: V1NotionScrapeResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerNotionScrape = (
-  body: V1NotionScrapeReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1NotionScrapeResp;
-    }
-  >({
-    path: `/api/v1/crawler/notion/scrape`,
+    path: `/api/v1/crawler/parse`,
     method: "POST",
     body: body,
     type: ContentType.Json,
@@ -405,7 +97,7 @@ export const postApiV1CrawlerNotionScrape = (
  */
 
 export const getApiV1CrawlerResult = (
-  query: GetApiV1CrawlerResultParams,
+  body: V1CrawlerResultReq,
   params: RequestParams = {},
 ) =>
   httpRequest<
@@ -415,7 +107,7 @@ export const getApiV1CrawlerResult = (
   >({
     path: `/api/v1/crawler/result`,
     method: "GET",
-    query: query,
+    body: body,
     type: ContentType.Json,
     format: "json",
     ...params,
@@ -446,306 +138,6 @@ export const postApiV1CrawlerResults = (
     path: `/api/v1/crawler/results`,
     method: "POST",
     body: param,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description Parse RSS
- *
- * @tags crawler
- * @name PostApiV1CrawlerRssParse
- * @summary Parse RSS
- * @request POST:/api/v1/crawler/rss/parse
- * @response `200` `(DomainPWResponse & {
-    data?: V1RssParseResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerRssParse = (
-  body: V1RssParseReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1RssParseResp;
-    }
-  >({
-    path: `/api/v1/crawler/rss/parse`,
-    method: "POST",
-    body: body,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description RSSScrape
- *
- * @tags crawler
- * @name PostApiV1CrawlerRssScrape
- * @summary RSSScrape
- * @request POST:/api/v1/crawler/rss/scrape
- * @response `200` `(DomainPWResponse & {
-    data?: V1RssScrapeResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerRssScrape = (
-  body: V1RssScrapeReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1RssScrapeResp;
-    }
-  >({
-    path: `/api/v1/crawler/rss/scrape`,
-    method: "POST",
-    body: body,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description Scrape
- *
- * @tags crawler
- * @name PostApiV1CrawlerScrape
- * @summary Scrape
- * @request POST:/api/v1/crawler/scrape
- * @response `200` `(DomainPWResponse & {
-    data?: V1ScrapeResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerScrape = (
-  body: V1ScrapeReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1ScrapeResp;
-    }
-  >({
-    path: `/api/v1/crawler/scrape`,
-    method: "POST",
-    body: body,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description Parse Sitemap
- *
- * @tags crawler
- * @name PostApiV1CrawlerSitemapParse
- * @summary Parse Sitemap
- * @request POST:/api/v1/crawler/sitemap/parse
- * @response `200` `(DomainPWResponse & {
-    data?: V1SitemapParseResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerSitemapParse = (
-  body: V1SitemapParseReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1SitemapParseResp;
-    }
-  >({
-    path: `/api/v1/crawler/sitemap/parse`,
-    method: "POST",
-    body: body,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description SitemapScrape
- *
- * @tags crawler
- * @name PostApiV1CrawlerSitemapScrape
- * @summary SitemapScrape
- * @request POST:/api/v1/crawler/sitemap/scrape
- * @response `200` `(DomainPWResponse & {
-    data?: V1SitemapScrapeResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerSitemapScrape = (
-  body: V1SitemapScrapeReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1SitemapScrapeResp;
-    }
-  >({
-    path: `/api/v1/crawler/sitemap/scrape`,
-    method: "POST",
-    body: body,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description Parse Siyuan Export File and return document list
- *
- * @tags crawler
- * @name PostApiV1CrawlerSiyuanParse
- * @summary SiyuanParse
- * @request POST:/api/v1/crawler/siyuan/parse
- * @response `200` `(DomainPWResponse & {
-    data?: V1SiyuanParseResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerSiyuanParse = (
-  data: PostApiV1CrawlerSiyuanParsePayload,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1SiyuanParseResp;
-    }
-  >({
-    path: `/api/v1/crawler/siyuan/parse`,
-    method: "POST",
-    body: data,
-    type: ContentType.FormData,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description Scrape specific Siyuan documents by ID
- *
- * @tags crawler
- * @name PostApiV1CrawlerSiyuanScrape
- * @summary SiyuanScrape
- * @request POST:/api/v1/crawler/siyuan/scrape
- * @response `200` `(DomainPWResponse & {
-    data?: V1SiyuanScrapeResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerSiyuanScrape = (
-  body: V1SiyuanScrapeReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1SiyuanScrapeResp;
-    }
-  >({
-    path: `/api/v1/crawler/siyuan/scrape`,
-    method: "POST",
-    body: body,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description Parse Wikijs Export File and return document list
- *
- * @tags crawler
- * @name PostApiV1CrawlerWikijsParse
- * @summary WikijsParse
- * @request POST:/api/v1/crawler/wikijs/parse
- * @response `200` `(DomainPWResponse & {
-    data?: V1WikijsParseResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerWikijsParse = (
-  data: PostApiV1CrawlerWikijsParsePayload,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1WikijsParseResp;
-    }
-  >({
-    path: `/api/v1/crawler/wikijs/parse`,
-    method: "POST",
-    body: data,
-    type: ContentType.FormData,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description Scrape specific Wikijs documents by ID
- *
- * @tags crawler
- * @name PostApiV1CrawlerWikijsScrape
- * @summary WikijsScrape
- * @request POST:/api/v1/crawler/wikijs/scrape
- * @response `200` `(DomainPWResponse & {
-    data?: V1WikijsScrapeResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerWikijsScrape = (
-  body: V1WikijsScrapeReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1WikijsScrapeResp;
-    }
-  >({
-    path: `/api/v1/crawler/wikijs/scrape`,
-    method: "POST",
-    body: body,
-    type: ContentType.Json,
-    format: "json",
-    ...params,
-  });
-
-/**
- * @description YuqueParse
- *
- * @tags crawler
- * @name PostApiV1CrawlerYuqueParse
- * @summary YuqueParse
- * @request POST:/api/v1/crawler/yuque/parse
- * @response `200` `(DomainPWResponse & {
-    data?: V1YuqueParseResp,
-
-})` OK
- */
-
-export const postApiV1CrawlerYuqueParse = (
-  body: V1YuqueParseReq,
-  params: RequestParams = {},
-) =>
-  httpRequest<
-    DomainPWResponse & {
-      data?: V1YuqueParseResp;
-    }
-  >({
-    path: `/api/v1/crawler/yuque/parse`,
-    method: "POST",
-    body: body,
     type: ContentType.Json,
     format: "json",
     ...params,
