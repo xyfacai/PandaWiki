ee4df8da626edf3511eb73c05b716d32588797dd
coltea <coltea@qq.com>
2025-11-03 20:51:29 +0800

    fix consumer deliver plicy

--- backend/mq/nats/consumer.go
+++ backend/mq/nats/consumer.go
@@ -96,6 +96,14 @@ func (c *MQConsumer) registerCoreNATSHandler(topic string, handler func(ctx cont
 func (c *MQConsumer) registerJetStreamHandler(topic string, handler func(ctx context.Context, msg types.Message) error) error {
 	consumerName := domain.TopicConsumerName[topic]
 
+	// Choose deliver policy based on topic
+	var deliverPolicy nats.SubOpt
+	if topic == domain.VectorTaskTopic {
+		deliverPolicy = nats.DeliverNew()
+	} else {
+		deliverPolicy = nats.DeliverAll()
+	}
+
 	sub, err := c.js.Subscribe(topic, func(msg *nats.Msg) {
 		c.logger.Debug("received message via JetStream",
 			log.String("topic", topic),
@@ -113,7 +121,7 @@ func (c *MQConsumer) registerJetStreamHandler(topic string, handler func(ctx con
 				log.String("topic", topic),
 				log.Error(err))
 		}
-	}, nats.DeliverAll(), nats.AckExplicit(), nats.Durable(consumerName), nats.ConsumerName(consumerName))
+	}, deliverPolicy, nats.AckExplicit(), nats.Durable(consumerName), nats.ConsumerName(consumerName))
 	if err != nil {
 		c.logger.Error("failed to subscribe to topic via JetStream",
 			log.String("topic", topic),
